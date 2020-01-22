b852936c659a23a8c1833a8fc9a7cef0d085fda6
ajronn <34678770+ajronn@users.noreply.github.com>
2020-01-22 12:10:11 +0100

    12

--- projekt/projekt/sklep/models.py
+++ projekt/projekt/sklep/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
-# Create your models here.
-class Klient():
-    idKlienta=models.IntegerField(primary_key=True);
-    imie=models.CharField(max_length=30);
-    nazwisko=models.CharField(max_length=30);
-    numerTelefonu=models.IntegerField();
+class Klient(models.Model):
+    idKlienta=models.IntegerField(primary_key=True)
+    imie=models.CharField(max_length=30)
+    nazwisko=models.CharField(max_length=30)
+    numerTelefonu=models.IntegerField()
 
-class Produkt():
-    idProduktu=models.IntegerField(primary_key=True);
-    rodzaj=models.CharField(max_length=30);
-    ilosc=models.IntegerField();
+class Produkt(models.Model):
+    idProduktu=models.IntegerField(primary_key=True)
+    rodzaj=models.CharField(max_length=30)
+    ilosc=models.IntegerField()
 
-class Zamowienie():
+class Zamowienie(models.Model):
     idZamowienia=models.IntegerField(primary_key=True)
     idKlienta=models.ForeignKey(Klient, on_delete=models.CASCADE)
     idProduktu=models.ForeignKey(Produkt, on_delete=models.CASCADE)
-    ilosc=models.IntegerField();
+    ilosc=models.IntegerField()
