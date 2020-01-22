9e7d43b4dc47d33fa997a740a4e636d009f5f3b1
kakapo97 <username@domaim.com>
2020-01-22 13:04:01 +0100

    wszystko gotowe

--- projekt/projekt/sklep/urls.py
+++ projekt/projekt/sklep/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('projekt/klienci', views.klienci_list),
     path('projekt/klienci/<int:pk>/',views.klient_detail),
 
-    path('mysite_kantor/zamowienia', views.zamowienia_list),
-    path('mysite_kantor/zamowienia/<int:pk>/', views.zamowienia_detail),
+    path('projekt/zamowienia', views.zamowienia_list),
+    path('projekt/zamowienia/<int:pk>/', views.zamowienia_detail),
 
-    path('mysite_kantor/produkty', views.produkty_list),
-    path('mysite_kantor/kurs_z/<int:pk>/', views.produkty_detail),
+    path('projekt/produkty', views.produkty_list),
+    path('projekt/produkty/<int:pk>/', views.produkty_detail),
 
     path('users/', views.UserList.as_view()),
     path('users/<int:pk>/', views.UserDetail.as_view()),
